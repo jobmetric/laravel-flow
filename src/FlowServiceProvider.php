f19aa7bfb7edeaf08a05702b8c89ba0b0965eddc
majid mohammadian <majeedmohammadian@gmail.com>
2023-11-15 15:12:09 +0330

    clean service provider

--- src/FlowServiceProvider.php
+++ src/FlowServiceProvider.php
@@ -23,23 +23,16 @@ class FlowServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerMigrations();
-        $this->registerPublishables();
+        if($this->app->runningInConsole()) {
+            // load migration
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+            // register publishable
+            $this->registerPublishables();
+        }
 
         // set translations
         $this->loadTranslationsFrom(realpath(__DIR__.'/../lang'), 'flow');
-    }
-
-    /**
-     * Register the Passport migration files.
-     *
-     * @return void
-     */
-    protected function registerMigrations(): void
-    {
-        if($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        }
     }
 
     /**
@@ -49,19 +42,17 @@ class FlowServiceProvider extends ServiceProvider
      */
     protected function registerPublishables(): void
     {
-        if($this->app->runningInConsole()) {
-            // run dependency publishable
-            $this->publishes(self::pathsToPublish(TranslationServiceProvider::class), 'translation');
+        // run dependency publishable
+        $this->publishes(self::pathsToPublish(TranslationServiceProvider::class), 'translation');
 
-            // publish config
-            $this->publishes([
-                realpath(__DIR__.'/../config/config.php') => config_path('workflow.php')
-            ], 'flow-config');
+        // publish config
+        $this->publishes([
+            realpath(__DIR__.'/../config/config.php') => config_path('workflow.php')
+        ], 'flow-config');
 
-            // publish migration
-            $this->publishes([
-                realpath(__DIR__.'/../database/migrations') => database_path('migrations')
-            ], 'flow-migrations');
-        }
+        // publish migration
+        $this->publishes([
+            realpath(__DIR__.'/../database/migrations') => database_path('migrations')
+        ], 'flow-migrations');
     }
 }
