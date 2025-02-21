3de95d04e64a42f3531542eeebfdf51e462b3f04
hassan7303 <hassanali7303@gmail.com>
2025-02-21 21:39:41 +0330

    added fa languege

--- lang/fa/base.php
+++ lang/fa/base.php
@@ -13,40 +13,40 @@ return [
     */
 
     'flow' => [
-        'exist'     => 'Flow driver `:driver` already exist.',
-        'inactive'  => 'Flow `:driver` is inactive.',
-        'not_found' => 'Flow driver `:driver` does not exist.',
+        'exist' => 'درایور فلو `:driver` قبلاً وجود دارد.',
+        'inactive' => 'فلو `:driver` غیرفعال است.',
+        'not_found' => 'درایور فلو `:driver` وجود ندارد.',
     ],
 
     'flow_state' => [
-        'start_type_is_exist'      => 'Flow state `start` type in `:driver` is exist.',
-        'invalid_type'             => 'Flow state type `:type` is invalid.',
-        'start_type_is_not_delete' => 'Flow state `start` type is not deletable.',
-        'start_type_is_not_change' => 'Flow state `start` type is not changeable.',
+        'start_type_is_exist' => 'نوع فلو استیت `start` در `:driver` وجود دارد.',
+        'invalid_type' => 'نوع فلو استیت `:type` معتبر نیست.',
+        'start_type_is_not_delete' => 'نوع فلو استیت `start` قابل حذف نیست.',
+        'start_type_is_not_change' => 'نوع فلو استیت `start` قابل تغییر نیست.',
     ],
 
     'flow_transition' => [
-        'slug_is_exist'                                         => 'Flow transition slug `:slug` is exist.',
-        'invalid'                                               => 'Flow transition is invalid.',
-        'state_start_not_in_to'                                 => 'Flow state start not in transition to.',
-        'state_end_not_in_from'                                 => 'Flow state end not in transition from.',
-        'from_not_set'                                          => 'Flow transition from is not set.',
-        'to_not_set'                                            => 'Flow transition to is not set.',
-        'state_driver_from_and_to_not_equal'                    => 'Flow transition state driver from and to must be equal.',
-        'exist'                                                 => 'Flow transition already exists.',
-        'from_state_start_not_move'                             => 'Flow transition from state start not move.',
-        'not_store_before_first_state'                          => 'Flow transition not store, Because there is no transition from the starting state.',
-        'have_at_least_one_transition_from_the_start_beginning' => 'Flow have at least one transition from the start beginning.',
+        'slug_is_exist' => 'اسلاگ انتقال فلو `:slug` وجود دارد.',
+        'invalid' => 'انتقال فلو نامعتبر است.',
+        'state_start_not_in_to' => 'فلو استیت شروع در انتقال به وجود ندارد.',
+        'state_end_not_in_from' => 'فلو استیت پایان در انتقال از وجود ندارد.',
+        'from_not_set' => 'انتقال فلو از تنظیم نشده است.',
+        'to_not_set' => 'انتقال فلو به تنظیم نشده است.',
+        'state_driver_from_and_to_not_equal' => 'درایور استیت فلو از و به باید برابر باشند.',
+        'exist' => 'انتقال فلو قبلاً وجود دارد.',
+        'from_state_start_not_move' => 'انتقال فلو از استیت شروع حرکت نمی‌کند.',
+        'not_store_before_first_state' => 'انتقال فلو ذخیره نمی‌شود، زیرا هیچ انتقالی از استیت شروع وجود ندارد.',
+        'have_at_least_one_transition_from_the_start_beginning' => 'فلو باید حداقل یک انتقال از شروع داشته باشد.',
     ],
 
     'flow_task' => [
-        'global' => 'Global',
-        'not_found' => 'flow task with id [:id] not found',
-        'task_driver_not_found' =>'flow task driver [:task] not found',
+        'global' => 'سراسری',
+        'not_found' => 'وظیفه فلو با شناسه [:id] پیدا نشد',
+        'task_driver_not_found' => 'درایور وظیفه فلو [:task] پیدا نشد',
     ],
 
     'validation' => [
-        'check_status_in_driver' => 'The selected status is invalid, you must select from (:status) items.',
-        'check_driver_exists' =>'The selected driver :driver does not exists',
+        'check_status_in_driver' => 'وضعیت انتخاب شده نامعتبر است، باید از موارد (:status) انتخاب کنید.',
+        'check_driver_exists' => 'درایور انتخاب شده :driver وجود ندارد',
     ],
 ];
