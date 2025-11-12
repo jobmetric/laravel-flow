8c5254a4420a33e61f51570ce2e0c60aa3d139e2
majidmohammadian <majeedmohammadian@gmail.com>
2025-11-12 23:42:07 +0330

    added runner method in FlowTransition for run any task in this transition

--- src/Services/FlowTransition.php
+++ src/Services/FlowTransition.php
@@ -2,16 +2,25 @@
 
 namespace JobMetric\Flow\Services;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
+use JobMetric\Flow\Contracts\AbstractTaskDriver;
+use JobMetric\Flow\DTO\ActionResult;
 use JobMetric\Flow\Enums\FlowStateTypeEnum;
 use JobMetric\Flow\Events\FlowTransition\FlowTransitionDeleteEvent;
 use JobMetric\Flow\Events\FlowTransition\FlowTransitionStoreEvent;
 use JobMetric\Flow\Events\FlowTransition\FlowTransitionUpdateEvent;
+use JobMetric\Flow\Exceptions\TaskRestrictionException;
+use JobMetric\Flow\Exceptions\TransitionNotFoundException;
 use JobMetric\Flow\Http\Requests\FlowTransition\StoreFlowTransitionRequest;
 use JobMetric\Flow\Http\Requests\FlowTransition\UpdateFlowTransitionRequest;
 use JobMetric\Flow\Http\Resources\FlowTransitionResource;
+use JobMetric\Flow\Models\FlowTask as FlowTaskModel;
 use JobMetric\Flow\Models\FlowTransition as FlowTransitionModel;
+use JobMetric\Flow\Support\FlowTaskContext;
+use JobMetric\Flow\Support\RestrictionResult;
 use JobMetric\PackageCore\Output\Response;
 use JobMetric\PackageCore\Services\AbstractCrudService;
 use RuntimeException;
@@ -94,7 +103,7 @@ class FlowTransition extends AbstractCrudService
         $transition = $model;
 
         $data = dto($data, UpdateFlowTransitionRequest::class, [
-            'flow_id' => $transition->flow_id,
+            'flow_id'            => $transition->flow_id,
             'flow_transition_id' => $transition->id,
         ]);
     }
@@ -117,21 +126,17 @@ class FlowTransition extends AbstractCrudService
                 ->with($with)
                 ->findOrFail($id);
 
-            $startStateId = $transition->flow
-                ->states()
+            $startStateId = $transition->flow->states()
                 ->where('type', FlowStateTypeEnum::START())
                 ->value('id');
 
-            if ($startStateId && (int)$transition->from === (int)$startStateId) {
-                $hasAnotherFromStart = $transition->flow
-                    ->transitions()
+            if ($startStateId && (int) $transition->from === (int) $startStateId) {
+                $hasAnotherFromStart = $transition->flow->transitions()
                     ->where('id', '!=', $transition->id)
                     ->exists();
 
                 if ($hasAnotherFromStart) {
-                    throw new RuntimeException(
-                        trans('workflow::errors.flow_transition.start_state_last_transition_delete')
-                    );
+                    throw new RuntimeException(trans('workflow::errors.flow_transition.start_state_last_transition_delete'));
                 }
             }
 
@@ -157,6 +162,7 @@ class FlowTransition extends AbstractCrudService
      *
      * @param Model $model
      * @param array<string,mixed> $data
+     *
      * @return void
      */
     protected function afterUpdate(Model $model, array &$data): void
@@ -168,10 +174,103 @@ class FlowTransition extends AbstractCrudService
      * Hook after destroy: invalidate caches.
      *
      * @param Model $model
+     *
      * @return void
      */
     protected function afterDestroy(Model $model): void
     {
         $this->forgetCache();
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function runner(int|string $key, array $payload = [], ?Authenticatable $user = null): ActionResult
+    {
+        $transition = FlowTransitionModel::query()
+            ->when(is_string($key), function ($query) use ($key) {
+                $query->where('slug', $key);
+            })
+            ->when(is_int($key), function ($query) use ($key) {
+                $query->where('id', $key);
+            })
+            ->with([
+                'flow',
+                'fromState',
+                'toState',
+                'tasks',
+            ])
+            ->firstOrFail();
+        
+        if (! $transition) {
+            throw new TransitionNotFoundException;
+        }
+
+        $actionResult = new ActionResult;
+        $context = new FlowTaskContext($transition->flow->subject_type, $actionResult, $payload, $user);
+
+        // Run restrictions tasks
+        foreach ($transition->tasks as $item) {
+            /** @var FlowTaskModel $item */
+            $driver = $item->driver;
+
+            if (is_null($driver)) {
+                continue;
+            }
+
+            if ($driver->taskType() == AbstractTaskDriver::TASK_TYPE_RESTRICTION) {
+                $context->replaceConfig($item->config);
+
+                /** @var RestrictionResult $result */
+                $result = $driver->restriction($context);
+
+                if (! $result->allowed()) {
+                    // Stop processing and return the restriction result
+                    throw new TaskRestrictionException($result->message() ?? null, $result->code() ?? 400);
+                }
+            }
+        }
+
+        // Run validations tasks
+        foreach ($transition->tasks as $item) {
+            /** @var FlowTaskModel $item */
+            $driver = $item->driver;
+
+            if (is_null($driver)) {
+                continue;
+            }
+
+            if ($driver->taskType() == AbstractTaskDriver::TASK_TYPE_VALIDATION) {
+                $context->replaceConfig($item->config);
+
+                $rules = $driver->rules($context);
+                $messages = $driver->messages($context);
+                $attributes = $driver->attributes($context);
+
+                $validator = validator($payload, $rules, $messages, $attributes);
+
+                if ($validator->fails()) {
+                    ValidationException::withMessages($validator->messages());
+                }
+            }
+        }
+
+        // Run actions tasks
+        foreach ($transition->tasks as $item) {
+            /** @var FlowTaskModel $item */
+            $driver = $item->driver;
+
+            if (is_null($driver)) {
+                continue;
+            }
+
+            if ($driver->taskType() == AbstractTaskDriver::TASK_TYPE_ACTION) {
+                $context->replaceConfig($item->config);
+
+                $driver->run($context);
+            }
+        }
+
+        return $actionResult;
     }
 }
