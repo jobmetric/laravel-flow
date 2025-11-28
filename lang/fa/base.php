60c752bdeea4625d79912fa1fac1e6e31a6afba6
majidmohammadian <majeedmohammadian@gmail.com>
2025-11-28 15:19:25 +0330

    removed FlowStateInvalidTypeException file and remove language key

--- lang/fa/base.php
+++ lang/fa/base.php
@@ -44,7 +44,6 @@ return [
 
     "flow_state" => [
         "start_type_is_exist" => "نوع فلو استیت `start` در `:driver` وجود دارد.",
-        "invalid_type" => "نوع فلو استیت `:type` معتبر نیست.",
         "start_type_is_not_delete" => "نوع فلو استیت `start` قابل حذف نیست.",
         "start_type_is_not_change" => "نوع فلو استیت `start` قابل تغییر نیست.",
     ],
